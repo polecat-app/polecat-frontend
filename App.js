cdf4c2d679b6e9e92e7548d981d9ea8ae117e706
yannick <yannickmacken@gmail.com>
2022-12-23 17:22:19 +0100

    new button

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>This is Yannicks app wow insane</Text>
+      <Button title='Tap me!'/>
     </View>
   );
 }
