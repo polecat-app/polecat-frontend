71e5c188c436983a4bf08e16fac416e9ff234525
yannick <yannickmacken@gmail.com>
2023-01-03 09:06:18 +0100

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is Yannicks app wow insane</Text>
+      <Text style={styles.text}>This is Yannicks app wow insane</Text>
       <Button title='Tap me!'/>
     </View>
   );
@@ -16,4 +16,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  // Text styling can be defined outside stylesheet object,
+  // but within object VScode will autocomplete
+  text: {
+    margin: 16,
+    padding: 16
+  }
 });
