450ba1a3b13b0cc6459a4cda4aae627568d4d6b6
yannick <yannickmacken@gmail.com>
2023-01-20 20:15:47 +0100

    update animal card

--- components/Animal.js
+++ components/Animal.js
@@ -1,37 +1,73 @@
 import { StyleSheet, Text, Pressable, View, Image } from "react-native";
 import cardStyle from "../styles/CardStyle";
+import { Ionicons } from '@expo/vector-icons'
+import { useState } from "react";
 
 function Animal(props) {
+
+  const [seen, setSeen] = useState(false)
+  const [love, setlove] = useState(false)
+  function toggleSeen() {
+    setSeen(!seen)
+  }
+  function toggleLove() {
+    setlove(!love)
+  }
+
   return (
-    <View style={containerStyle.container}>
-        <View style={modalStyle.centeredView}>
-            <View style={modalStyle.modalView}>
-            <View style={{...cardStyle.textContainer, height: undefined}}>
-                <Text style={cardStyle.commonName}>{props.commonName}</Text>
-                <Text style={cardStyle.binomial}>{props.binomial}</Text>
-                <Text style={cardStyle.summary}>{props.summary}</Text>
-								<Pressable
-									style={[modalStyle.button, modalStyle.buttonClose]}
-									onPress={() => props.setShowAnimal(false)}
-								>
-									<Text style={modalStyle.textStyle}>Hide Modal</Text>
-								</Pressable>
-            </View>
-            <Image style={cardStyle.image} source={{uri: props.image}}/>
-            </View>
-        </View>
+  <View style={modalStyle.modalView}>
+
+    {/* Image */}
+    <View style={{backgroundColor:'black'}}>
+      <Image style={modalStyle.image} source={{uri: props.image}}/>
     </View>
+
+    {/* Text */}
+    <View style={modalStyle.titleText}>
+      <Text style={cardStyle.commonName}>{props.commonName}</Text>
+      <Text style={cardStyle.binomial}>{props.binomial}</Text>
+      <Text style={cardStyle.summary}>{props.summary}</Text>
+    </View>
+
+    {/* Bottom bar */}
+    <View style={modalStyle.bottomBar}>
+      <Pressable onPress={() => toggleSeen()}>
+        <Ionicons 
+          name={seen? "eye" : "eye-outline"} 
+          size={32} 
+          style={seen? modalStyle.seenSelected : modalStyle.seenUnselected} />
+      </Pressable>
+      <Pressable onPress={() => toggleLove()}>
+        <Ionicons 
+          name={love? "heart" : "heart-outline"} 
+          size={32} 
+          style={love? modalStyle.loveSelected : modalStyle.loveUnselected} />
+      </Pressable>
+      <Pressable
+        style={[modalStyle.button, modalStyle.buttonClose]}
+        onPress={() => props.setShowAnimal(false)}
+      >
+        <Text style={modalStyle.textStyle}>Close</Text>
+      </Pressable>
+    </View>
+
+  </View>
   );
 };
 
-const containerStyle = StyleSheet.create({
-  container: {
-      marginHorizontal: 10,
-      flex: 1,
-  }
-})
-
 const modalStyle = StyleSheet.create({
+  image: {
+    resizeMode: 'contain',
+    height: 260,
+    width: '100%',
+  },
+  titleText: {
+    padding: 10,
+    flexDirection: 'column',
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start'
+  },
   button: {
     borderRadius: 20,
     padding: 10,
@@ -47,13 +83,11 @@ const modalStyle = StyleSheet.create({
   },
   modalView: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: 'column',
     justifyContent: 'space-between',
-    alignItems: 'center',
     margin: 10,
     backgroundColor: "white",
     borderRadius: 20,
-    padding: 20,
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -63,12 +97,22 @@ const modalStyle = StyleSheet.create({
     shadowRadius: 4,
     elevation: 5
   },
-  centeredView: {
-    paddingVertical: 10,
-    flex: 1,
+  bottomBar: {
+    padding: 10,
     flexDirection: 'row',
-    justifyContent: "center",
-    alignItems: "stretch",
+    justifyContent: 'space-around',
+  },
+  seenUnselected: {
+    color: '#aaa',
+  },
+  seenSelected: {
+    color: 'teal',
+  },
+  loveUnselected: {
+    color: '#aaa',
+  },
+  loveSelected: {
+    color: 'red',
   },
 })
 
