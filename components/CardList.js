721fae74baca31cdd028bd02e5c0fe75ca3272c9
yannick <yannickmacken@gmail.com>
2023-01-27 18:20:22 +0100

    filterbar

--- components/CardList.js
+++ components/CardList.js
@@ -1,6 +1,6 @@
-import { getCurrentPositionAsync, useForegroundPermissions, PermissionStatus } from "expo-location"
-import { Alert, ScrollView, StyleSheet } from "react-native"
+import { Alert, ScrollView, StyleSheet, View } from "react-native"
 import AnimalCard from "./Card"
+import FilterBar from "./FilterBar"
 
 
 // Get animals
@@ -34,53 +34,19 @@ This sparrow-sized bird has the typical short-tailed, large-headed kingfisher pr
 
 
 function CardList(props) {
-
-  // Get location
-  const [locationPermissionInformation, requestPermission] = useForegroundPermissions()
-
-  async function verifyPermissions() {
-    if (locationPermissionInformation === null) {
-      return false
-    }
-    if (locationPermissionInformation.status === PermissionStatus.UNDETERMINED) {
-      const permissionResponse = await requestPermission()
-
-      return permissionResponse.granted
-    }
-    if (locationPermissionInformation.status === PermissionStatus.DENIED) {
-      Alert.alert('You need to grant location permissions to find local wildlife.')
-      return false
-    }
-    if (locationPermissionInformation.status === PermissionStatus.GRANTED) {
-      return true
-    }
-    return false
-  }
-
-  async function getLocationHandler() {
-    const hasPermission = await verifyPermissions()
-
-    if (!hasPermission) {
-      return
-    }
-    const location = await getCurrentPositionAsync()
-    console.log(location)
-
-  }
-
-  getLocationHandler()
-
-  // Component
   return (
-    <ScrollView style={styles.scrollViewContainer}>
-        {animals.map((animal) => (
-          <AnimalCard
-            key={animal.key}
-            {...animal} 
-            {...props}
-            />
-        ))}
-    </ScrollView>
+    <View style={{flexDirection: 'column', width:'100%', flex:1}}>
+      <FilterBar/>
+      <ScrollView style={styles.scrollViewContainer}>
+          {animals.map((animal) => (
+            <AnimalCard
+              key={animal.key}
+              {...animal}
+              {...props}
+              />
+          ))}
+      </ScrollView>
+    </View>
   )
 }
 
@@ -89,5 +55,6 @@ export default CardList
 const styles = StyleSheet.create({
     scrollViewContainer: {
         marginHorizontal: 10,
+        flex:10
     }
   })
---
+++ components/FilterBar.js
@@ -0,0 +1,60 @@
+import { Alert, StyleSheet, View, Text } from "react-native"
+import { getCurrentPositionAsync, useForegroundPermissions, PermissionStatus } from "expo-location"
+import { useEffect, useState } from "react"
+
+
+
+function FilterBar() {
+
+  const [location, setLocation] = useState({})
+
+  // Get location
+  const [locationPermissionInformation, requestPermission] = useForegroundPermissions()
+
+  async function verifyPermissions() {
+    if (locationPermissionInformation === null) {
+      return false
+    }
+    if (locationPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission()
+
+      return permissionResponse.granted
+    }
+    if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert('You need to grant location permissions to find local wildlife.')
+      return false
+    }
+    if (locationPermissionInformation.status === PermissionStatus.GRANTED) {
+      return true
+    }
+    return false
+  }
+
+  async function getLocationHandler() {
+    const hasPermission = await verifyPermissions()
+
+    if (!hasPermission) {
+      return
+    }
+    const location = await getCurrentPositionAsync()
+    setLocation(location)
+
+  }
+  
+  useEffect(() => {
+    getLocationHandler()
+  })
+  console.log(location.timestamp)
+
+  return (
+    <View style={{padding: 20, height: 150, backgroundColor: 'teal', justifyContent: 'space-around', flexDirection: 'column'}}>
+      <Text 
+        style={{color: 'white', flex:1, fontSize: 25}} 
+        numberOfLines={1}>
+      {location.timestamp}
+      </Text>
+    </View>
+  )
+}
+
+export default FilterBar
