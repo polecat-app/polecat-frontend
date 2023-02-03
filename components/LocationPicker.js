97788b7fbd29050f6049a8a3c8555ff01fc3b632
yannick <yannickmacken@gmail.com>
2023-02-03 10:29:17 +0100

    custom hook for get location, remove static mapview

--- components/LocationPicker.js
+++ components/LocationPicker.js
@@ -1,9 +1,8 @@
-import { Pressable, StyleSheet, Text, Alert, Image, View } from "react-native"
+import { Pressable, StyleSheet, Text } from "react-native"
 import { Ionicons } from "@expo/vector-icons";
-import { getCurrentPositionAsync, useForegroundPermissions, PermissionStatus } from 'expo-location'
 import { useState } from "react";
-import getMapPreview from "../util/location";
 import { useNavigation } from "@react-navigation/native"
+import useLocation from "../hooks/useLocation";
 
 
 function LocationPicker() {
@@ -11,87 +10,22 @@ function LocationPicker() {
   const [pickedLocation, setPickedLocation] = useState(null)
   const navigation = useNavigation()
 
-  const [
-    locationPermissionInformation, 
-    requestPermission
-  ] = useForegroundPermissions()
-  
-  async function verifyPermissions() {
-    if (locationPermissionInformation.status === PermissionStatus.UNDETERMINED) {
-      const permissionResponse = await requestPermission()
-      return permissionResponse.granted
-    }
-    if (locationPermissionInformation.status === PermissionStatus.DENIED) {
-      Alert.alert('You need to grant location permissions to find local wildlife.')
-      return false
-    }
-    return true
-  }
-  
-
-  async function getLocationHandler() {
-    console.log('pressed')
-    const hasPermission = await verifyPermissions()
-    console.log('permission', hasPermission)
-    if (!hasPermission) { return }
-    const location = await getCurrentPositionAsync()
-    setPickedLocation({
-      lat: location.coords.latitude,
-      lng: location.coords.longitude
-    })
-    console.log('location', location)
-  }
+  const location = useLocation()
+  console.log(location)
 
   function pickOnMapHandler() {
     navigation.navigate('Map', {pickedLocation})
   }
 
-  let locationPreview = <Text>No location picked yet.</Text>
-
-  if (pickedLocation) {
-    console.log('picked location', pickedLocation)
-    console.log('getmappreview')
-    const uri = getMapPreview(pickedLocation.lat, pickedLocation.lng)
-    console.log(uri)
-    locationPreview = (
-      <Image 
-        style={styles.mapPreviewImage} 
-        source={{
-          uri: uri
-        }}
-      />
-    )
-  }
-
   return (
-    <View>
-      <View style={styles.mapPreview}>
-        {locationPreview}
-      </View>
-      <View style={styles.actions}>
-        <Pressable onPress={getLocationHandler}>
-          <Text>Get location</Text>
-          <Ionicons name="ios-location" size={15} color={'white'} />
-        </Pressable>
-        <Pressable onPress={pickOnMapHandler}>
-          <Text>Pick on map</Text>
-          <Ionicons name="ios-map" size={15} color={'white'} />
-        </Pressable>
-      </View>
-    </View>
+    <Pressable onPress={pickOnMapHandler}>
+      <Text>{location? 'some location' : 'Loading location...'}</Text>
+      <Ionicons name="ios-map" size={15} color={'white'} />
+    </Pressable>
   )
 }
 
 const styles = StyleSheet.create({
-  mapPreview: {
-    marginVertical: 8,
-    width: '100%',
-    height: 200,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'grey',
-    borderRadius: 5
-  },
   actions: {
     flexDirection: 'row',
     justifyContent: 'space-around',
--- util/location.js
+++
@@ -1,8 +0,0 @@
-import { GOOGLE_API_KEY } from 'react-native-dotenv'
-
-function getMapPreview(lat, lng) {
-  const mapPreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=13&size=400x200&maptype=terrain&markers=color:red%7C${lat},${lng}&key=${GOOGLE_API_KEY}`
-  return mapPreviewUrl
-}
-
-export default getMapPreview
