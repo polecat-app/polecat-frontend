a11b672c1c173e0dcb4b06871dc640102631e7d0
yannick <yannickmacken@gmail.com>
2023-02-05 14:57:58 +0100

    working search screen

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,123 +1,94 @@
-import React, { useState } from "react";
-import {
-  View,
-  TextInput,
-  StyleSheet,
-  ImageBackground,
-  Text,
-  TouchableOpacity,
-  FlatList,
-} from "react-native";
+import React from "react";
+import { StyleSheet, TextInput, View, Keyboard, Button } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import { Colors } from "../styles/Colors";
 import { Offsets } from "../styles/Offsets";
-import textStyles from "../styles/TextStyles";
-import { Ionicons } from "@expo/vector-icons";
 
-const SearchBar = ({ onSearch }) => {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [showResults, setShowResults] = useState(false);
-  const [results, setResults] = useState([]);
-
-  const handleSearch = (text) => {
-    setSearchTerm(text);
-    onSearch(text);
-    // Perform search logic here, such as making an API call
-    // and updating the results in state
-  };
-
-  const closeSearch = () => {
-    setShowResults(false);
-    setSearchTerm("");
-  };
-
+const SearchBar = (props) => {
   return (
     <View style={styles.container}>
-      <ImageBackground
-        source={require("../images/background-image.jpg")}
-        style={styles.backgroundImage}
-      />
-      <View style={styles.onImage}>
-        <Text style={textStyles.overlayBold}>Search Animals</Text>
-        <View style={styles.inputBar}>
+      <View
+        style={
+          !props.clicked
+            ? styles.searchBar__unclicked
+            : styles.searchBar__clicked
+        }
+      >
+        <Ionicons
+          name="ios-search"
+          size={20}
+          color={Colors.Inactive}
+          style={{ marginLeft: 1 }}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Search"
+          value={props.searchPhrase}
+          onChangeText={props.setSearchPhrase}
+          onFocus={() => {
+            props.setClicked(true);
+          }}
+        />
+
+        {props.clicked && (
           <Ionicons
-            style={styles.searchIcon}
-            name="ios-search"
+            name="ios-close"
             size={20}
             color={Colors.Inactive}
-          />
-          <TextInput
-            style={[textStyles.basic, styles.input]}
-            value={searchTerm}
-            onChangeText={handleSearch}
-            onFocus={() => setShowResults(true)}
-            placeholder="Name, binominal..."
-          />
-          {showResults && (
-            <TouchableOpacity onPress={closeSearch}>
-              <Text style={textStyles.basic}>Close</Text>
-            </TouchableOpacity>
-          )}
-        </View>
-        {showResults && (
-          <FlatList
-            data={results}
-            keyExtractor={(item) => item.id}
-            renderItem={({ item }) => (
-              <TouchableOpacity>
-                <Text style={{ padding: 10 }}>{item.name}</Text>
-              </TouchableOpacity>
-            )}
+            style={{ padding: 1 }}
+            onPress={() => {
+              props.setSearchPhrase("");
+            }}
           />
         )}
       </View>
+      {props.clicked && (
+        <View>
+          <Button
+            title="Cancel"
+            onPress={() => {
+              Keyboard.dismiss();
+              props.setClicked(false);
+            }}
+          ></Button>
+        </View>
+      )}
     </View>
   );
 };
 
+export default SearchBar;
+
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "column",
-    flex: 1,
-    alignItems: "center",
-    width: "100%",
-  },
-  onImage: {
-    padding: Offsets.DefaultMargin,
-    paddingTop: 30,
-    flex: 1,
-    width: "100%",
-    position: "absolute",
-    zIndex: 6,
-    alignItems: "center",
-    flexDirection: "column",
+    marginTop: Offsets.DefaultMargin,
     justifyContent: "flex-start",
-  },
-  inputBar: {
-    width: "90%",
-    marginTop: 10,
-    zIndex: 6,
-    backgroundColor: Colors.Primary,
-    borderRadius: Offsets.BorderRadius,
-    shadowColor: "black",
-    shadowOpacity: 0.4,
-    shadowRadius: Offsets.DefaultMargin,
     alignItems: "center",
     flexDirection: "row",
+    marginLeft: 30,
+    marginRight: 30,
   },
-  searchIcon: {
-    margin: Offsets.DefaultMargin,
+  searchBar__unclicked: {
+    padding: Offsets.DefaultMargin,
+    flexDirection: "row",
+    width: "100%",
+    backgroundColor: Colors.Primary,
+    opacity: 0.7,
+    borderRadius: Offsets.BorderRadius,
+    alignItems: "center",
+  },
+  searchBar__clicked: {
+    padding: Offsets.DefaultMargin,
+    flexDirection: "row",
+    width: "80%",
+    backgroundColor: Colors.Primary,
+    borderRadius: Offsets.BorderRadius,
+    alignItems: "center",
   },
   input: {
-    margin: Offsets.DefaultMargin,
-    width: "100%",
+    fontSize: 20,
+    marginLeft: Offsets.DefaultMargin,
     flex: 1,
-    textAlign: "Left",
-  },
-  backgroundImage: {
-    flex: 1,
-    resizeMode: "contain",
-    width: "100%",
   },
 });
 
-export default SearchBar;
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,9 +1,88 @@
-import { Text, View } from "react-native";
-import * as React from "react";
-import SearchBar from "../components/SearchBar";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  ActivityIndicator,
+  View,
+  ImageBackground,
+} from "react-native";
 
-export function SearchScreen() {
+import SearchList from "../components/SearchList";
+import SearchBar from "../components/SearchBar";
+import textStyles from "../styles/TextStyles";
+import { Offsets } from "../styles/Offsets";
+
+function SearchScreen() {
+  const [searchPhrase, setSearchPhrase] = useState("");
+  const [clicked, setClicked] = useState(false);
+  const [fakeData, setFakeData] = useState();
+
+  // get data from the fake api
+  useEffect(() => {
+    const getData = async () => {
+      const apiResponse = await fetch(
+        "https://my-json-server.typicode.com/kevintomas1995/logRocket_searchBar/languages"
+      );
+      const data = await apiResponse.json();
+      setFakeData(data);
+    };
+    getData();
+  }, []);
+
   return (
-    <SearchBar onSearch={() => {}}></SearchBar>
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("../images/background-image.jpg")}
+        style={styles.backgroundImage}
+      />
+      <View style={{...styles.onImage}}>
+        {!clicked && (
+          <Text style={[styles.title, textStyles.overlayBold]}>
+            Search Animals
+          </Text>
+        )}
+
+        <SearchBar
+          searchPhrase={searchPhrase}
+          setSearchPhrase={setSearchPhrase}
+          clicked={clicked}
+          setClicked={setClicked}
+        />
+        {!fakeData && (<ActivityIndicator size="large" />)}
+        {(fakeData && clicked) && (
+          <SearchList
+            searchPhrase={searchPhrase}
+            data={fakeData}
+            setClicked={setClicked}
+          />
+        )}
+      </View>
+
+    </View>
   );
 }
+
+export default SearchScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    width:"100%",
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: "contain",
+    width: "100%",
+  },
+  onImage: {
+    height: "100%",
+    alignItems: "center",
+    position: "absolute",
+    zIndex: 5,
+    margin: Offsets.DefaultMargin,
+    flex:1,
+  },
+  title: {
+    marginTop: "65%",
+  },
+});
