2c1c68ccbc6b60d160f809a5baad8e541a918d0e
yannick <yannickmacken@gmail.com>
2023-02-10 18:32:21 +0100

    fake api call to get animals, single filter state

--- components/AnimalList.js
+++ components/AnimalList.js
@@ -1,6 +1,6 @@
 import { useIsFocused } from "@react-navigation/native";
 import { useEffect, useState } from "react";
-import { ScrollView, StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, View, Text } from "react-native";
 import useLocation from "../hooks/useLocation";
 import { Offsets } from "../styles/Offsets";
 import AnimalCard from "./AnimalCard";
@@ -10,43 +10,20 @@ import SearchBar from "./SearchBar";
 import getAddressFromCoordinates from "../util/GetAddress";
 import SavedBar from "./SavedBar";
 import { Colors } from "../styles/Colors";
-
-// Get animals
-const animals = [
-  {
-    key: 0,
-    binomial: "Mustela Putorius",
-    commonName: "European Polecat",
-    summary: `The European polecat (Mustela putorius), also known as the common polecat, 
-black polecat, or forest polecat, is a species of mustelid native to western Eurasia and North 
-Africa. It is of a generally dark brown colour, with a pale underbelly and a dark mask across 
-the face. Occasionally, colour mutations including albinos, leucists, isabellinists, 
-xanthochromists, amelanists and erythrists occur.[2] It has a shorter, more compact body 
-than other Mustela species,[3] a more powerfully built skull and dentition,[4] is less agile,[5] 
-and is well known for having the characteristic ability to secrete a 
-particularly foul-smelling liquid to mark its territory.`,
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/1/17/Storm_the_polecat.jpg",
-    tags: ["mammal", "endangered"],
-  },
-  {
-    key: 1,
-    binomial: "Alcedo atthis",
-    commonName: "Common Kingfisher",
-    summary: `The common kingfisher (Alcedo atthis), also known as the Eurasian kingfisher and river kingfisher, 
-is a small kingfisher with seven subspecies recognized within its wide distribution across Eurasia and North Africa. It is resident in much of its range, but migrates from areas where rivers freeze in winter.
-This sparrow-sized bird has the typical short-tailed, large-headed kingfisher profile; it has blue upperparts, orange underparts and a long bill. It feeds mainly on fish, caught by diving, and has special visual adaptations to enable it to see prey under water. The glossy white eggs are laid in a nest at the end of a burrow in a riverbank.`,
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/9/92/%E2%99%82_Common_Kingfisher_%28Alcedo_atthis%29_Photograph_By_Shantanu_Kuveskar%2C_Mangaon%2C_Maharashtra%2C_India.jpg",
-    tags: ["bird", "rare"],
-  },
-];
+import { getAnimals } from "../util/AnimalAPI";
 
 function AnimalList({ navigation, route }) {
   // Filter states
   const [selected, setSelected] = useState([]);
   const isFocused = useIsFocused();
   const [selectedBar, setSelectedBar] = useState(Bars.FilterBar);
+  const [filterProps, setFilterProps] = useState({
+    commonName: null,
+    tags: null,
+    liked: null,
+    seen: null,
+    location: null,
+  });
 
   // Location states
   const location = useLocation();
@@ -56,6 +33,13 @@ function AnimalList({ navigation, route }) {
   // Search states
   const [searchPhrase, setSearchPhrase] = useState("");
   const [clicked, setClicked] = useState(false);
+
+  // Saved states
+  const [savedFilterState, setSavedFilterState] = useState(null);
+
+  // List states
+  const [loading, setLoading] = useState(false);
+  const [animals, setAnimals] = useState([]);
 
   useEffect(() => {
     if (isFocused && route.params) {
@@ -87,6 +71,47 @@ function AnimalList({ navigation, route }) {
     }
   }, [pickedLocation]);
 
+  // Update search query
+  useEffect(() => {
+    setFilterProps({
+      commonName: null,
+      tags: selected,
+      liked: null,
+      seen: null,
+      location: pickedLocation,
+    });
+  }, [selected, pickedLocation]);
+  useEffect(() => {
+    setFilterProps({
+      commonName: searchPhrase,
+      tags: null,
+      liked: null,
+      seen: null,
+      location: null,
+    });
+  }, [searchPhrase]);
+  useEffect(() => {
+    setFilterProps({
+      commonName: null,
+      tags: null,
+      liked: savedFilterState === "liked" ? true : null,
+      seen: savedFilterState === "seen" ? true : null,
+      location: null,
+    });
+  }, [savedFilterState]);
+
+  // Timeout and send request for animal list after delay
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      getAnimals({
+        setLoading: setLoading,
+        setAnimals: setAnimals,
+        filterProps: filterProps,
+      });
+    }, 2000);
+    return () => clearTimeout(delayDebounceFn);
+  }, [filterProps]);
+
   return (
     <View style={{ flexDirection: "column", width: "100%", flex: 1 }}>
       <View style={styles.barContainer}>
@@ -99,7 +124,12 @@ function AnimalList({ navigation, route }) {
             setSelectedBar={setSelectedBar}
           ></SearchBar>
         )}
-        {selectedBar === Bars.SavedBar && <SavedBar setSelectedBar={setSelectedBar}></SavedBar>}
+        {selectedBar === Bars.SavedBar && (
+          <SavedBar
+            setSelectedBar={setSelectedBar}
+            setSavedFilterState={setSavedFilterState}
+          ></SavedBar>
+        )}
         {selectedBar === Bars.FilterBar && (
           <FilterBar
             selected={selected}
@@ -110,6 +140,7 @@ function AnimalList({ navigation, route }) {
           />
         )}
       </View>
+      <Text>{loading && "loading.."}</Text>
       <ScrollView style={styles.scrollViewContainer}>
         {animals.map((animal) => (
           <AnimalCard key={animal.key} {...animal} />
